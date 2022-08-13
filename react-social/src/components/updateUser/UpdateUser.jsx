3f846e497b99be289114950bcdb048b6ec2367cf
sumit <sumitmondal9448@gmail.com>
2022-08-14 01:11:55 +0530

    img upload step2

--- react-social/src/components/updateUser/UpdateUser.jsx
+++ react-social/src/components/updateUser/UpdateUser.jsx
@@ -61,6 +61,22 @@ export const UpdateUser = ({ userc }) => {
       console.log(file)
       setImageAsFile(imageAsFile => (image))
       try {
+        const uploadTask = storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile)
+        uploadTask.on('state_changed',
+          (snapShot) => {
+            //takes a snap shot of the process as it is happening
+            console.log(snapShot)
+          }, (err) => {
+            //catches the errors
+            console.log(err)
+          }, () => {
+            // gets the functions from storage refences the image storage in firebase by the children
+            // gets the download url then sets the image from firebase as the value for the imgUrl key:
+            storage.ref('images').child(imageAsFile.name).getDownloadURL()
+              .then(fireBaseUrl => {
+                setImageAsUrl(prevObject => ({ ...prevObject, imgUrl: fireBaseUrl }))
+              })
+          })
         await axiosInstance.post("/upload", data);
       } catch (err) {
         console.log(err);
