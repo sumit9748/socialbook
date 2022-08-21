ca2178a4ba53379c7c153cd356d757765a46e8c5
sumit <sumitmondal9448@gmail.com>
2022-08-22 00:52:25 +0530

    change

--- react-social/src/apiCalls.js
+++ react-social/src/apiCalls.js
@@ -2,23 +2,29 @@ import { axiosInstance } from "./config";
 import Snackbar from '@mui/material/Snackbar';
 import { Alert } from "@mui/material";
 import React from "react";
+import Stack from '@mui/material/Stack';
 
 export const loginCall = async (userCredential, dispatch) => {
     dispatch({ type: "LOGIN_START" });
 
-    const res = await axiosInstance.post("auth/login", userCredential).then((res) => {
-        <Snackbar open={true} autoHideDuration={6000}>
-            <Alert severity="success" sx={{ width: '100%' }}>
-                {`${res}`}
-            </Alert>
-        </Snackbar>
+    await axiosInstance.post("auth/login", userCredential).then((res) => {
+        <Stack spacing={2} sx={{ width: '100%' }}>
+            <Snackbar open={true} autoHideDuration={6000}>
+                <Alert severity="success" sx={{ width: '100%' }}>
+                    {`${res}`}
+                </Alert>
+            </Snackbar>
+        </Stack>
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     }).catch((err) => {
-        <Snackbar open={true} autoHideDuration={6000}>
-            <Alert severity="success" sx={{ width: '100%' }}>
-                {`${err}`}
-            </Alert>
-        </Snackbar>
+        <Stack spacing={2} sx={{ width: '100%' }}>
+
+            <Snackbar open={true} autoHideDuration={6000}>
+                <Alert severity="error" sx={{ width: '100%' }}>
+                    {`${err}`}
+                </Alert>
+            </Snackbar>
+        </Stack>
         dispatch({ type: "LOGIN_FAILURE", payload: err });
 
     })
