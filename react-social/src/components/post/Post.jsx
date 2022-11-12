bcda3f65ba0ee0c68a50f1f6aa3ec04c5130f4e2
sumit <sumitmondal9448@gmail.com>
2022-11-12 23:46:20 +0530

    comment

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -211,6 +211,12 @@ export const AccordianComments = ({ post, socket, currentUser }) => {
             <img
               src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
               alt=""
+              style={{
+                width: "15px",
+                height: "15px",
+                borderRadius: "50%",
+                objectFit: "cover",
+              }}
             />
             <p>13th Oct</p>
           </div>
